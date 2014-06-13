CyanogenMod/android_device_sony_taoshan

taoshan: use our own adreno blobs
Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

BoardConfig.mk
@@ -50,6 +50,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1711276032
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 
+BOARD_EGL_CFG := device/sony/nicki/rootdir/system/lib/egl/egl.cfg
+
 BOARD_VOLD_MAX_PARTITIONS := 33
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true

taoshanROMs/device_sony_taoshan-old

BoardConfig.mk
@@ -50,6 +50,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1711276032
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 
+BOARD_EGL_CFG := device/sony/taoshan/rootdir/system/lib/egl/egl.cfg
+
 BOARD_VOLD_MAX_PARTITIONS := 33
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true